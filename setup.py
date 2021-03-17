d5c1fcbeb985190e472669172ecfa733dd03e26c
gaijin01mx <80802900+gaijin01mx@users.noreply.github.com>
2021-03-16 22:39:14 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#from distutils.core import setup
+from setuptools import setup
+
+long_description = open('README.md').read()
+
+setup(
+    name='Roll4Me',
+    version='1.0',
+    license='GPL3',
+    author = 'Victor Caldas',
+    author_email = 'caldas.victor@gmail.com',
+    traductor='gaijin01mx'
+    url = 'https://github.com/vcaldas/Roll4Me',
+    download_url = 'https://github.com/vcaldas/Roll4Me',
+    keywords = ['dnd', 'dice', 'wod'],
+    description= 'A simple telegram dice bot using standard dice notation',
+    long_description=long_description,
+    scripts=[
+        'bot',
+    ]
+)
